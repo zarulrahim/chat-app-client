cf35dfc344b919116e70ed60a5a819ce85e7bc66
zarulrahim <zarulizham97@gmail.com>
2022-05-30 07:36:46 +0800

    Update

--- src/screens/adminDashboard/index.js
+++ src/screens/adminDashboard/index.js
@@ -13,6 +13,8 @@ import { colors } from '../../themes';
 
 const AdminDashboardScreen = observer((props) => {
   const navigate = useNavigate();
+  const [users, setUsers] = useState([]);
+  const [keyword, setKeyword] = useState("");
   const [currentScreen, setCurrentScreen] = useState('default'); // default | register | edit
   const [formState, setFormState] = useState({
     first_name: "",
@@ -24,7 +26,13 @@ const AdminDashboardScreen = observer((props) => {
   })
   
   useEffect(() => {
-    mstAuth.fetchUser();
+    mstAuth.fetchUser()
+    .then((response) => {
+      setUsers(response)
+    })
+    .catch((error) => {
+      alert(error)
+    })
   }, [])
 
   const onChangeScreen = (screen) => {
@@ -102,95 +110,8 @@ const AdminDashboardScreen = observer((props) => {
     })
   }
 
-  const UserListingComponent = (props) => {
-    return (
-      <List 
-        className={styles.list}
-        dataSource={props.users}
-        header={
-          <div className={styles.listHeader}>
-            <div className={styles.wrapper}>
-              <Input placeholder='Search User' className={styles.searchBox} />
-              <Button className={styles.button} type='link' onClick={() => { onChangeScreen('register') }}>
-                <FiUserPlus size={28} color={colors.primary} />
-              </Button>
-            </div>
-          </div>
-        }
-        renderItem={(user, index) => {
-          return (
-            <Card
-              bodyStyle={customStyles.list.cardBody}
-            >
-              <div className={styles.listItem}>
-                <div className={styles.listItem}>
-                  <div>{user.email}</div>
-                </div>
-                <div className={styles.listItem}>
-                  <Button className={styles.button} style={{ marginRight: 15 }} type='link' onClick={() => { props.onGotoUser(user) }}>
-                    <FiUser size={22} color={colors.primary} />
-                  </Button>
-                  <Button className={styles.button} style={{ marginRight: 15 }}  type='link' onClick={() => { props.onEditUser(user) }}>
-                    <FiEdit size={20} color={colors.primary}/>
-                  </Button>
-                  <Button className={styles.button} style={{ marginRight: 0 }} type='link' onClick={() => { props.onRemoveUser(user) }}>
-                    <FiTrash2 size={20} color={colors.primary} />
-                  </Button>
-                </div>
-              </div>
-            </Card>
-          )
-        }}
-      />
-    )
-  }
-
-  const UserRegisterComponent = (props) => {
-    return (
-      <div className={styles.login}>
-        <div className={styles.flexContainer}>
-          <Input className={styles.inputText} size="large" style={{marginRight: 5}} placeholder='First Name' onChange={(e) => { props.onChangeInput(e.target.value, "first_name")  }} />
-          <Input className={styles.inputText} size="large" style={{marginLeft: 5}} placeholder='Last Name' onChange={(e) => { props.onChangeInput(e.target.value, "last_name")  }} />
-        </div>
-        <div className={styles.flexContainer}>
-          <Input className={styles.inputText} size="large" style={{marginRight: 5}} placeholder='Email' onChange={(e) => { props.onChangeInput(e.target.value, "email")  }} />
-          <Input className={styles.inputText} size="large" style={{marginLeft: 5}} placeholder='Phone' onChange={(e) => { props.onChangeInput(e.target.value, "phone_number")  }} />
-        </div>
-        <div className={styles.flexContainer}>
-          <DatePicker className={styles.datePicker} size="large" placeholder='Date of Birth' onChange={(date, dateString) => { props.onChangeInput(dateString, "dob") }} />
-        </div>
-        <div className={styles.flexContainer}>
-          <Input.Password className={styles.inputText} size="large" placeholder='Password' onChange={(e) => { props.onChangeInput(e.target.value, "password")  }} />
-        </div>
-        <div className={styles.flexContainer}>
-          <Button className={styles.primaryBtn} type='link' size='large' onClick={() => { props.onRegister() }}>Add User</Button>
-        </div>
-      </div>
-    )
-  }
-
-  const UserEditComponent = (props) => {
-    return (
-      <div className={styles.login}>
-        <div className={styles.flexContainer}>
-          <Input defaultValue={props.formState.first_name} className={styles.inputText} size="large" style={{marginRight: 5}} placeholder='First Name' onChange={(e) => { props.onChangeInput(e.target.value, "first_name")  }} />
-          <Input defaultValue={props.formState.last_name} className={styles.inputText} size="large" style={{marginLeft: 5}} placeholder='Last Name' onChange={(e) => { props.onChangeInput(e.target.value, "last_name")  }} />
-        </div>
-        <div className={styles.flexContainer}>
-          <Input defaultValue={props.formState.email} className={styles.inputText} size="large" style={{marginRight: 5}} placeholder='Email' onChange={(e) => { props.onChangeInput(e.target.value, "email")  }} />
-          <Input defaultValue={props.formState.phone_number} className={styles.inputText} size="large" style={{marginLeft: 5}} placeholder='Phone' onChange={(e) => { props.onChangeInput(e.target.value, "phone_number")  }} />
-        </div>
-        <div className={styles.flexContainer}>
-          <DatePicker defaultValue={moment(props.formState.dob, "YYYY-MM-DD")} className={styles.datePicker} size="large" placeholder='Date of Birth' onChange={(date, dateString) => { props.onChangeInput(dateString, "dob") }} />
-        </div>
-        <div className={styles.flexContainer}>
-          <Input.Password className={styles.inputText} size="large" placeholder='Password' onChange={(e) => { props.onChangeInput(e.target.value, "password")  }} />
-        </div>
-        <div className={styles.flexContainer}>
-          <Button className={styles.primaryBtn} type='link' size='large' onClick={() => { props.onUpdate() }}>Update User</Button>
-        </div>
-      </div>
-    )
+  const onSearchUser = (keyword) => {
+    setKeyword(keyword)
   }
 
   return (
@@ -203,15 +124,88 @@ const AdminDashboardScreen = observer((props) => {
         {
           currentScreen === 'default' ?
           (
-            <UserListingComponent users={getSnapshot(mstAuth).users || []} navigate={navigate} onEditUser={onEditUser} onRemoveUser={onRemoveUser} onGotoUser={onGotoUser} />
+            <List 
+              className={styles.list}
+              dataSource={users.filter(i => i.email.includes(keyword))}
+              header={
+                <div className={styles.listHeader}>
+                  <div className={styles.wrapper}>
+                    <Input placeholder='Search User' className={styles.searchBox} onChange={(e) => { onSearchUser(e.target.value)  }} />
+                    <Button className={styles.button} type='link' onClick={() => { onChangeScreen('register') }}>
+                      <FiUserPlus size={28} color={colors.primary} />
+                    </Button>
+                  </div>
+                </div>
+              }
+              renderItem={(user, index) => {
+                return (
+                  <Card
+                    bodyStyle={customStyles.list.cardBody}
+                  >
+                    <div className={styles.listItem}>
+                      <div className={styles.listItem}>
+                        <div>{user.email}</div>
+                      </div>
+                      <div className={styles.listItem}>
+                        <Button className={styles.button} style={{ marginRight: 15 }} type='link' onClick={() => { onGotoUser(user) }}>
+                          <FiUser size={22} color={colors.primary} />
+                        </Button>
+                        <Button className={styles.button} style={{ marginRight: 15 }}  type='link' onClick={() => { onEditUser(user) }}>
+                          <FiEdit size={20} color={colors.primary}/>
+                        </Button>
+                        <Button className={styles.button} style={{ marginRight: 0 }} type='link' onClick={() => { onRemoveUser(user) }}>
+                          <FiTrash2 size={20} color={colors.primary} />
+                        </Button>
+                      </div>
+                    </div>
+                  </Card>
+                )
+              }}
+            />
           )
           : currentScreen === 'register' ?
           (
-            <UserRegisterComponent formState={formState} onChangeInput={onChangeInput} onRegister={onRegister} />
+            <div className={styles.login}>
+              <div className={styles.flexContainer}>
+                <Input className={styles.inputText} size="large" style={{marginRight: 5}} placeholder='First Name' onChange={(e) => { onChangeInput(e.target.value, "first_name")  }} />
+                <Input className={styles.inputText} size="large" style={{marginLeft: 5}} placeholder='Last Name' onChange={(e) => { onChangeInput(e.target.value, "last_name")  }} />
+              </div>
+              <div className={styles.flexContainer}>
+                <Input className={styles.inputText} size="large" style={{marginRight: 5}} placeholder='Email' onChange={(e) => { onChangeInput(e.target.value, "email")  }} />
+                <Input className={styles.inputText} size="large" style={{marginLeft: 5}} placeholder='Phone' onChange={(e) => { onChangeInput(e.target.value, "phone_number")  }} />
+              </div>
+              <div className={styles.flexContainer}>
+                <DatePicker className={styles.datePicker} size="large" placeholder='Date of Birth' onChange={(date, dateString) => { onChangeInput(dateString, "dob") }} />
+              </div>
+              <div className={styles.flexContainer}>
+                <Input.Password className={styles.inputText} size="large" placeholder='Password' onChange={(e) => { onChangeInput(e.target.value, "password")  }} />
+              </div>
+              <div className={styles.flexContainer}>
+                <Button className={styles.primaryBtn} type='link' size='large' onClick={() => { onRegister() }}>Add User</Button>
+              </div>
+            </div>
           )
           : currentScreen === 'edit' ?
           (
-            <UserEditComponent formState={formState} onChangeInput={onChangeInput} onUpdate={onUpdate} />
+            <div className={styles.login}>
+              <div className={styles.flexContainer}>
+                <Input defaultValue={formState.first_name} className={styles.inputText} size="large" style={{marginRight: 5}} placeholder='First Name' onChange={(e) => { onChangeInput(e.target.value, "first_name")  }} />
+                <Input defaultValue={formState.last_name} className={styles.inputText} size="large" style={{marginLeft: 5}} placeholder='Last Name' onChange={(e) => { onChangeInput(e.target.value, "last_name")  }} />
+              </div>
+              <div className={styles.flexContainer}>
+                <Input defaultValue={formState.email} className={styles.inputText} size="large" style={{marginRight: 5}} placeholder='Email' onChange={(e) => { onChangeInput(e.target.value, "email")  }} />
+                <Input defaultValue={formState.phone_number} className={styles.inputText} size="large" style={{marginLeft: 5}} placeholder='Phone' onChange={(e) => { onChangeInput(e.target.value, "phone_number")  }} />
+              </div>
+              <div className={styles.flexContainer}>
+                <DatePicker defaultValue={moment(formState.dob, "YYYY-MM-DD")} className={styles.datePicker} size="large" placeholder='Date of Birth' onChange={(date, dateString) => { onChangeInput(dateString, "dob") }} />
+              </div>
+              <div className={styles.flexContainer}>
+                <Input.Password className={styles.inputText} size="large" placeholder='Password' onChange={(e) => { onChangeInput(e.target.value, "password")  }} />
+              </div>
+              <div className={styles.flexContainer}>
+                <Button className={styles.primaryBtn} type='link' size='large' onClick={() => { onUpdate() }}>Update User</Button>
+              </div>
+            </div>
           )
           :
           (
